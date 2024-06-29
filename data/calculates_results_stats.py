3bba22c8791016344712b619eb571f6884c7f556
irc-chris <ishawniachristopher@gmail.com>
2024-06-29 18:36:44 -0500

    todo 5 finished

--- data/calculates_results_stats.py
+++ data/calculates_results_stats.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
-#                                                                             
-# PROGRAMMER:
-# DATE CREATED:                                  
-# REVISED DATE: 
-# PURPOSE: Create a function calculates_results_stats that calculates the 
-#          statistics of the results of the programrun using the classifier's model 
-#          architecture to classify the images. This function will use the 
-#          results in the results dictionary to calculate these statistics. 
+#
+# PROGRAMMER: Ishawnia Christopher
+# DATE CREATED: June 29, 2024
+# REVISED DATE:
+# PURPOSE: Create a function calculates_results_stats that calculates the
+#          statistics of the results of the programrun using the classifier's model
+#          architecture to classify the images. This function will use the
+#          results in the results dictionary to calculate these statistics.
 #          This function will then put the results statistics in a dictionary
 #          (results_stats_dic) that's created and returned by this function.
-#          This will allow the user of the program to determine the 'best' 
+#          This will allow the user of the program to determine the 'best'
 #          model for classifying the images. The statistics that are calculated
 #          will be counts and percentages. Please see "Intro to Python - Project
-#          classifying Images - xx Calculating Results" for details on the 
-#          how to calculate the counts and percentages for this function.    
+#          classifying Images - xx Calculating Results" for details on the
+#          how to calculate the counts and percentages for this function.
 #         This function inputs:
-#            -The results dictionary as results_dic within calculates_results_stats 
+#            -The results dictionary as results_dic within calculates_results_stats
 #             function and results for the function call within main.
 #         This function creates and returns the Results Statistics Dictionary -
-#          results_stats_dic. This dictionary contains the results statistics 
-#          (either a percentage or a count) where the key is the statistic's 
-#           name (starting with 'pct' for percentage or 'n' for count) and value 
-#          is the statistic's value.  This dictionary should contain the 
+#          results_stats_dic. This dictionary contains the results statistics
+#          (either a percentage or a count) where the key is the statistic's
+#           name (starting with 'pct' for percentage or 'n' for count) and value
+#          is the statistic's value.  This dictionary should contain the
 #          following keys:
 #            n_images - number of images
 #            n_dogs_img - number of dog images
@@ -39,37 +39,37 @@
 #
 ##
 # TODO 5: Define calculates_results_stats function below, please be certain to replace None
-#       in the return statement with the results_stats_dic dictionary that you create 
+#       in the return statement with the results_stats_dic dictionary that you create
 #       with this function
-# 
+#
 def calculates_results_stats(results_dic):
     """
-    Calculates statistics of the results of the program run using classifier's model 
-    architecture to classifying pet images. Then puts the results statistics in a 
+    Calculates statistics of the results of the program run using classifier's model
+    architecture to classifying pet images. Then puts the results statistics in a
     dictionary (results_stats_dic) so that it's returned for printing as to help
-    the user to determine the 'best' model for classifying images. Note that 
+    the user to determine the 'best' model for classifying images. Note that
     the statistics calculated as the results are either percentages or counts.
     Parameters:
-      results_dic - Dictionary with key as image filename and value as a List 
+      results_dic - Dictionary with key as image filename and value as a List
              (index)idx 0 = pet image label (string)
                     idx 1 = classifier label (string)
-                    idx 2 = 1/0 (int)  where 1 = match between pet image and 
+                    idx 2 = 1/0 (int)  where 1 = match between pet image and
                             classifer labels and 0 = no match between labels
-                    idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and 
-                            0 = pet Image 'is-NOT-a' dog. 
-                    idx 4 = 1/0 (int)  where 1 = Classifier classifies image 
-                            'as-a' dog and 0 = Classifier classifies image  
+                    idx 3 = 1/0 (int)  where 1 = pet image 'is-a' dog and
+                            0 = pet Image 'is-NOT-a' dog.
+                    idx 4 = 1/0 (int)  where 1 = Classifier classifies image
+                            'as-a' dog and 0 = Classifier classifies image
                             'as-NOT-a' dog.
     Returns:
      results_stats_dic - Dictionary that contains the results statistics (either
-                    a percentage or a count) where the key is the statistic's 
+                    a percentage or a count) where the key is the statistic's
                      name (starting with 'pct' for percentage or 'n' for count)
                      and the value is the statistic's value. See comments above
                      and the previous topic Calculating Results in the class for details
                      on how to calculate the counts and statistics.
-    """        
-    # Replace None with the results_stats_dic dictionary that you created with 
-    # this function 
+    """
+    # Replace None with the results_stats_dic dictionary that you created with
+    # this function
     results_stats_dict = {}
 
     # Z = number of images
@@ -82,37 +82,37 @@ def calculates_results_stats(results_dic):
     C = 0
     # E = number of correct breed matches
     E = 0
-    # Y = number of pet label being in classified labels 
+    # Y = number of pet label being in classified labels
     Y = 0
-    
+
     # Iterate through pet images
     for key in results_dic:
         value = results_dic[key]
-        if value[3]==1 and value[4]==1: # Dog and Classified as Dog
+        if value[3] == 1 and value[4] == 1:  # Dog and Classified as Dog
             A += 1
-        if value[3]==1: # Dog
+        if value[3] == 1:  # Dog
             B += 1
-        if value[2]==1 and value[3]==1: # Dog Correct breed
+        if value[2] == 1 and value[3] == 1:  # Dog Correct breed
             E += 1
-        if value[3]==0 and value[4]==0: # Not Dog and Classified as Not Dog
+        if value[3] == 0 and value[4] == 0:  # Not Dog and Classified as Not Dog
             C += 1
-        if value[2]==1: # Correct label regardless of dog
+        if value[2] == 1:  # Correct label regardless of dog
             Y += 1
 
     # Store values
-    results_stats_dict['n_images'] = Z
-    results_stats_dict['n_dogs_img'] = B
+    results_stats_dict["n_images"] = Z
+    results_stats_dict["n_dogs_img"] = B
     if Z > 0:
         # D = number of not dogs (pet labels)
         n_pet_notd = Z - B
-        results_stats_dict['n_notdogs_img'] = n_pet_notd
-        results_stats_dict['pct_corr_label'] = Y/Z * 100
+        results_stats_dict["n_notdogs_img"] = n_pet_notd
+        results_stats_dict["pct_corr_label"] = Y / Z * 100
     if B > 0:
-        results_stats_dict['pct_correct_dogs'] = A/B * 100
-        results_stats_dict['pct_correct_breed'] = E/B * 100
+        results_stats_dict["pct_correct_dogs"] = A / B * 100
+        results_stats_dict["pct_correct_breed"] = E / B * 100
     if n_pet_notd > 0:
-        results_stats_dict['pct_correct_notdogs'] = C/n_pet_notd * 100
-    results_stats_dict['n_corr_dog'] = A
-    results_stats_dict['n_corr_breed'] = E
+        results_stats_dict["pct_correct_notdogs"] = C / n_pet_notd * 100
+    results_stats_dict["n_corr_dog"] = A
+    results_stats_dict["n_corr_breed"] = E
 
     return results_stats_dict
--- data/get_pet_labels.py
+++ data/get_pet_labels.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
-#          the image's filename. This function inputs: 
-#           - The Image Folder as image_dir within get_pet_labels function and 
-#             as in_arg.dir for the function call within the main function. 
+#
+# PROGRAMMER: Ishawnia Christopher
+# DATE CREATED: June 27, 2024
+# REVISED DATE:
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from
+#          the image's filename. This function inputs:
+#           - The Image Folder as image_dir within get_pet_labels function and
+#             as in_arg.dir for the function call within the main function.
 #          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main. 
+#          within get_pet_labels function and as results within main.
 #          The results_dic dictionary has a 'key' that's the image filename and
 #          a 'value' that's a list. This list will contain the following item
 #          at index 0 : pet image label (string).
@@ -19,15 +19,16 @@
 # Imports python modules
 from os import listdir
 
+
 # TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
+#       in the return statement with results_dic dictionary that you create
 #       with this function
-# 
+#
 def get_pet_labels(image_dir):
     """
-    Creates a dictionary of pet labels (results_dic) based upon the filenames 
-    of the image files. These pet image labels are used to check the accuracy 
-    of the labels that are returned by the classifier function, since the 
+    Creates a dictionary of pet labels (results_dic) based upon the filenames
+    of the image files. These pet image labels are used to check the accuracy
+    of the labels that are returned by the classifier function, since the
     filenames of the images contain the true identity of the pet in the image.
     Be sure to format the pet labels so that they are in all lower case letters
     and with leading and trailing whitespace characters stripped from them.
@@ -36,7 +37,7 @@ def get_pet_labels(image_dir):
      image_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
     Returns:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a 
+      results_dic - Dictionary with 'key' as image filename and 'value' as a
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
@@ -47,30 +48,32 @@ def get_pet_labels(image_dir):
 
     # Iterate through all files in the directory
     for file_name in all_files:
+        # Check it's not a duplicate label
+        if file_name not in results_dic:
+            word_list = file_name.split("_")
+            label = ""
 
-      # Check it's not a duplicate label
-      if file_name not in results_dic:
-        word_list = file_name.split('_')
-        label = ""
+            # Check all alphabetical and get spaces
+            for word in word_list:
+                if word.isalpha():
+                    label += word + " "
 
-        # Check all alphabetical and get spaces
-        for word in word_list:
-          if word.isalpha():
-              label += word + " "
-        
-        # Tried to Get name before numbers and convert to lowercase, but that relies
-        # on correct input formatting
-        #         label = file_name[:file_name.rfind('_')].lower()
-        
-        # Separate by spaces and remove any extra external spacing
-        label = label.strip()
+            # Tried to Get name before numbers and convert to lowercase, but that relies
+            # on correct input formatting
+            #         label = file_name[:file_name.rfind('_')].lower()
 
-        # Store lowercase label in dictionary
-        results_dic[file_name] = [label.lower()]
+            # Separate by spaces and remove any extra external spacing
+            label = label.strip()
+
+            # Store lowercase label in dictionary
+            results_dic[file_name] = [label.lower()]
     else:
-         print("** Warning: Key=", file_name, 
-               "already exists in results_dic with value =", 
-               results_dic[file_name])
-      
+        print(
+            "** Warning: Key=",
+            file_name,
+            "already exists in results_dic with value =",
+            results_dic[file_name],
+        )
+
     # Return dictionary
     return results_dic
